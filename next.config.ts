11c0f4ef7960056914f90d9721f89b7480df5354
Antony Lambi <antony.lambi88@gmail.com>
2025-08-26 20:43:22 +0000

    toujours rien , regarde https://tehen1.github.io/cyberstride-app/

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/cyberstride-app',
   output: 'export',
   typescript: {
     ignoreBuildErrors: true,
